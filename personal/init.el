31eecb775b0a5aa8acf1bd770116efb3e06015c3
Yasuhito Takamiya <yasuhito@gmail.com>
2015-09-11 16:08:21 +0900

    use-package 化

--- personal/init.el
+++ personal/init.el
@@ -1,4 +1,10 @@
-(require 'org)
-(require 'ob-tangle)
+(require 'use-package)
 
-(org-babel-load-file (expand-file-name "key-bindings.org"))
+(use-package org)
+(use-package ob-tangle)
+
+(defun my/load-org-file (file)
+  "Load up FILE (*.org) in prelude-personal-dir"
+  (org-babel-load-file (expand-file-name file prelude-personal-dir)))
+
+(my/load-org-file "key-bindings.org")
