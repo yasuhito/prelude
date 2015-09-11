836349a7fb70744c7164c8456b66323345247f1c
Yasuhito Takamiya <yasuhito@gmail.com>
2015-09-11 16:19:01 +0900

    .elの編集でflycheckがコメントの警告を出さないようにする。

--- personal/custom.el
+++ personal/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flycheck-disabled-checkers (quote (emacs-lisp-checkdoc)))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
